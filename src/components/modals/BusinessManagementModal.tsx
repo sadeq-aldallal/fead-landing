1c66fedd4375ef5c089859931f27d55384ace6a8
Sadeq Aldallal <sadeq.aldallal@outlook.com>
2025-08-22 22:14:47 +0200

    Fix Business Management Modal Width Properly

--- src/components/modals/BusinessManagementModal.tsx
+++ src/components/modals/BusinessManagementModal.tsx
@@ -158,7 +158,7 @@ export const BusinessManagementModal: React.FC<BusinessManagementModalProps> = (
 
   return (
     <div className="modal-overlay">
-      <div className="modal-content max-w-4xl">
+      <div className="modal-content max-w-6xl">
         <button
           onClick={onClose}
           className="modal-close"
